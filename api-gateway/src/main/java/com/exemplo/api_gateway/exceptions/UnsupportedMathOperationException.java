5a6b72c900c9fc49df9bd285a09f81119a329942
joaomarquardt <marquardt1402@gmail.com>
2024-09-06 17:23:37 -0300

    feat: add exceptions in order to handle errors in math operations

---
+++ api-gateway/src/main/java/com/exemplo/api_gateway/exceptions/UnsupportedMathOperationException.java
@@ -0,0 +1,28 @@
+package com.exemplo.api_gateway.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsupportedMathOperationException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public UnsupportedMathOperationException() {
+    }
+
+    public UnsupportedMathOperationException(String message) {
+        super(message);
+    }
+
+    public UnsupportedMathOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UnsupportedMathOperationException(Throwable cause) {
+        super(cause);
+    }
+
+    public UnsupportedMathOperationException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
