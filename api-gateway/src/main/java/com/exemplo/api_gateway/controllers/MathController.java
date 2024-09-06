b9667f5e2e4167fbf8e58ad75862c5e6354022ad
joaomarquardt <marquardt1402@gmail.com>
2024-09-06 19:12:01 -0300

    refactor: separate math methods into simple math class

--- api-gateway/src/main/java/com/exemplo/api_gateway/controllers/MathController.java
+++ api-gateway/src/main/java/com/exemplo/api_gateway/controllers/MathController.java
@@ -1,6 +1,8 @@
-package com.exemplo.api_gateway;
+package com.exemplo.api_gateway.controllers;
 
+import com.exemplo.api_gateway.converters.NumberConverter;
 import com.exemplo.api_gateway.exceptions.UnsupportedMathOperationException;
+import com.exemplo.api_gateway.math.SimpleMath;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -8,65 +10,51 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 @RequestMapping("/math")
 public class MathController {
-    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private SimpleMath math = new SimpleMath();
 
     @GetMapping(value = "/addition/{numberOne}/{numberTwo}")
     public Double addition(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Os valores devem se restringir apenas à números.");
         }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return math.addition(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @GetMapping(value = "/subtraction/{numberOne}/{numberTwo}")
     public Double subtraction(@PathVariable(value = "numberOne") String numberOne,
                               @PathVariable(value = "numberTwo") String numberTwo) throws Exception{
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Os valores devem se restringir apenas à números.");
         }
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return math.subtraction(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @GetMapping(value = "multiplication/{numberOne}/{numberTwo}")
     public Double multiplication(@PathVariable(value = "numberOne") String numberOne,
                            @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Os valores devem se restringir apenas à números.");
         }
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return math.multiplication(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @GetMapping(value = "division/{numberOne}/{numberTwo}")
     public Double division(@PathVariable(value = "numberOne") String numberOne,
                            @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if(!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Os valores devem se restringir apenas à números.");
         }
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return math.division(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
     @GetMapping(value = "squareroot/{number}")
     public Double squareroot(@PathVariable(value = "number") String number) throws Exception {
-        if(!isNumeric(number)) {
+        if(!NumberConverter.isNumeric(number)) {
             throw new UnsupportedMathOperationException("Os valores devem se restringir apenas à números.");
         }
-        return Math.sqrt(convertToDouble(number));
+        return math.squareroot(NumberConverter.convertToDouble(number));
     }
-
-
-    public Double convertToDouble(String strNumber) {
-        String number = strNumber.replaceAll(",", ".");
-        return Double.parseDouble(number);
-    }
-
-    public boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
-
-
 }
